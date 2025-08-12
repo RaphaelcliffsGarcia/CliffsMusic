243555830da77c8aa4d367fdf3c300a343ff17c9
RaphaelcliffsGarcia <cliffsdesigner@gmail.com>
2025-08-12 19:10:08 -0300

    Adicionando Routes e refatorando o codigo + data base

--- front_end/src/components/Header.jsx
+++ front_end/src/components/Header.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import logoCliffs from '../assets/logo/cliffs-logo.png';
+import React from "react";
+import logoCliffs from "../assets/logo/cliffs-logo.png";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='header'>
+    <div className="header">
+      <Link to="/">
         <img src={logoCliffs} alt="Logo do cliffsFy" />
-        <a className='header__link' href="">
-            <h1>CLIFFSFY</h1>
-        </a>
+      </Link>
+      <Link to="/" className="header__link">
+        <h1>CLIFFSFY</h1>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
